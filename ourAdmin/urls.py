95b0ad7b07689a6eed6bd45a20be892d742e5252
andreacent <andreacent8@gmail.com>
2017-05-04 20:26:15 -0400

    [week1] Product search template created

--- ourAdmin/urls.py
+++ ourAdmin/urls.py
@@ -3,6 +3,8 @@ from ourAdmin.views import *
 
 urlpatterns = [
 	# PROD
+    url(r'^prodsearch/$', ProdSearch.as_view(), name='ProdSearch'),
+    url(r'^prodsearchajax/$', ProdSearchAjax.as_view(), name='ProdSearchAjax'),
 	url(r'^prodcreate/$', ProdCreateModify.as_view(), name='ProdCreate'),
     url(r'^prodmodify/(?P<pk>\d+)/$', ProdCreateModify.as_view(), name='ProdModify'),
 ]
--- ourAdmin/views.py
+++ ourAdmin/views.py
@@ -1,13 +1,47 @@
 # -*- coding: utf-8 -*-
+import json
 # Django
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 #from django.contrib import messages
 #from django.core.urlresolvers import reverse
 #from braces.views import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 # App
 from ourAdmin.models import Prod
 from ourAdmin.forms import ProdForm
+
+class ProdSearch(TemplateView):
+    """
+    Search a Prod 
+    """
+
+    template_name = 'ourAdmin/prod/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProdSearch, self).get_context_data(**kwargs)
+        context['Title'] = "Buscar producto"
+        return context
+
+class ProdSearchAjax(View):
+    """
+    Get and send ajax with the product searching
+    """
+
+    def post(self, request, *args, **kwargs):
+        post_values = request.POST.copy()
+        prods = []
+        if 'id' in post_values:
+            try:
+                prods = [Prod.objects.get(id=int(post_values['id']))]
+            except:
+                pass
+        elif 'name' in post_values and not prods:
+            prods = Prod.objects.filter(name__icontains=int(post_values['name']))
+
+        data = [{'pk': prod.id, 'name': prod.name} for prod in prods]
+        return HttpResponse(json.dumps(data), content_type='application/json')
+
 
 class ProdCreateModify(TemplateView):
     """
@@ -19,7 +53,7 @@ class ProdCreateModify(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(ProdCreateModify, self).get_context_data(**kwargs)
         if 'pk' in kwargs:
-            prod = get_object_or_404(Prod, id=int(post_values['pk']))
+            prod = get_object_or_404(Prod, id=int(kwargs['pk']))
             context['form'] = ProdForm(instance=prod)
             context['Title'] = "Modificar Prod"
         else:
@@ -31,15 +65,17 @@ class ProdCreateModify(TemplateView):
     def post(self, request, *args, **kwargs):
         post_values = request.POST.copy()
         if 'pk' in kwargs:
-            prod = get_object_or_404(Prod, id=int(post_values['pk']))
+            prod = get_object_or_404(Prod, id=int(kwargs['pk']))
             form = ProdForm(post_values,instance=prod)
+            title = "Modificar Prod"
         else:
             form = ProdForm(post_values)
+            title = "Crear Prod"
 
         if form.is_valid():
             prod = form.save()
         else:
-            return render_to_response(template_name, {'form':form},
+            return render_to_response(template_name, {'form':form,'Title':title},
                                       context_instance=RequestContext(request))
 
-        return redirect('ProdCreate')  #cambiar por Producto 001
+        return redirect('ProdSearch')  
