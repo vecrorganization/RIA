513fd5924852c414f62254442ea85ec8343b2d7a
andreacent <andreacent8@gmail.com>
2017-07-10 18:59:28 -0400

    reset password

--- ourAuth/urls.py
+++ ourAuth/urls.py
@@ -11,4 +11,38 @@ urlpatterns = [
     url(r'^account_activation_sent/$', account_activation_sent, name='account_activation_sent'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         activate, name='activate'),
+
+    ## RESET PASSWORD
+    url(r'^password/reset/$',
+        auth_views.password_reset,
+        {
+            'post_reset_redirect': 'password_reset_done',
+            'template_name': 'password/password_reset_form.html',
+            'email_template_name': 'password/password_reset_email.html',
+            'subject_template_name': 'password/password_reset_subject.txt',
+            'from_email':'equipo@RIA.com'
+        },
+        name="password_reset"
+    ),
+    url(
+        r'^password/reset/done/$',
+        auth_views.password_reset_done,
+        {'template_name': 'password/password_reset_done.html'},
+        name="password_reset_done"
+    ),
+    url(
+        r'^password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        auth_views.password_reset_confirm,
+        {
+            'post_reset_redirect': 'password_done',
+            'template_name': 'password/password_reset_confirm.html'
+        },
+        name="password_reset_confirm"
+    ),
+    url(
+        r'^password/done/$',
+        auth_views.password_reset_complete,
+        {'template_name': 'password/password_reset_complete.html'},
+        name='password_done'
+    ),
 ]
