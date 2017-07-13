33ebb11d1d19adb4e94c65b4afe5c06be02a4ba5
Isaac Gonzalez <isaacg311@gmail.com>
2017-07-12 23:50:03 -0400

    heroku db

--- ourSite/urls.py
+++ ourSite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import patterns
 #from django.contrib import admin
 
 urlpatterns = [
