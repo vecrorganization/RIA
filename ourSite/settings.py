eeedbdf6777aed96d5a3198b19a649f04f69116e
Isaac Gonzalez <isaacg311@gmail.com>
2017-07-13 00:16:50 -0400

    heroku db

--- ourSite/settings.py
+++ ourSite/settings.py
@@ -78,24 +78,25 @@ WSGI_APPLICATION = 'ourSite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', 
+            'NAME': 'db_ria',
+            'USER': 'admin_ria',
+            'PASSWORD': 'ria123',
+            'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+            'PORT': '3306',
+            'OPTIONS': {
+                'sql_mode': 'traditional',
+            }
+        }
+    }
+
 if 'JAWSDB_URL' in os.environ:
     db_from_env = dj_database_url.config()
     DATABASES = { 'default': dj_database_url.config() } 
-else:
-
-    DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.mysql', 
-                'NAME': 'db_ria',
-                'USER': 'admin_ria',
-                'PASSWORD': 'ria123',
-                'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-                'PORT': '3306',
-                'OPTIONS': {
-                    'sql_mode': 'traditional',
-                }
-            }
-        }
 
 
 # Password validation
