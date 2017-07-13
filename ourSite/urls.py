4b68925e1e204a2a7f141ecb72ca4f908e5dff54
Isaac Gonzalez <isaacg311@gmail.com>
2017-07-13 00:13:50 -0400

    heroku statics

--- ourSite/urls.py
+++ ourSite/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls.defaults import patterns
 #from django.contrib import admin
 
 urlpatterns = [
